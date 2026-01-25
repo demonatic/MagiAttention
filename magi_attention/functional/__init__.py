c55c502b83df3623d8e24c4d9db87444715ee6a5
auto_user[Jager(Jager)] <jager@minimaxi.com>
2026-01-25 21:15:24 +0800

    adaptive max func + revert correct_attn_fwd_result
    Combined changes from:
    - d4d8f80d884: adaptive max func
    - 2a6d120: revert correct_attn_fwd_result

--- magi_attention/functional/__init__.py
+++ magi_attention/functional/__init__.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from .dispatch import dispatch_func, undispatch_func
 from .dist_attn import dist_attn_func
 from .fa4 import ffa_fa4_func
@@ -30,22 +28,3 @@ __all__ = [
     "correct_attn_out",
     "correct_attn_lse",
 ]
-
-
-def fa4_hsfu_max_num_funcs() -> int:
-    """
-    HACK: since the FA4 with the attention mask representation of HSFU Functions
-    requires the maximum number of functions to be set,
-    and it is hard to pass through the user API to the arguments,
-    thus we let user set it through the environment variable, if using FA4 backend.
-
-    NOTE: this is a beta feature, under development
-    """
-
-    max_num_funcs = int(os.environ.get("MAGI_ATTENTION_FA4_HSFU_MAX_NUM_FUNCS", "0"))
-
-    if max_num_funcs == 0:
-        raise ValueError("MAGI_ATTENTION_FA4_HSFU_MAX_NUM_FUNCS is not set")
-    assert max_num_funcs % 2 == 1, "MAGI_ATTENTION_FA4_HSFU_MAX_NUM_FUNCS must be odd"
-
-    return max_num_funcs
